b31e323d703bc38cec74e29d0371225722765af9
Jatin975 <71510945+Jatin975@users.noreply.github.com>
2022-04-26 12:39:53 +0530

    Add files via upload

---
+++ R/SumFile.R
@@ -0,0 +1,12 @@
+
+#'This function adds two numbers and returns the result
+#'@export
+#'@param x numeric variable
+#'@param y numeric variable
+#'@return return numeric variable
+
+AddNum <- function(x, y){
+  result <- x+y
+  return(result)
+}
+
